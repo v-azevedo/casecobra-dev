e37c497c7d74047dd893cde654238a9b68858382
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-06-12 15:33:46 -0300

    Fix const models wrong export

--- src/validators/option-validator.ts
+++ src/validators/option-validator.ts
@@ -23,37 +23,35 @@ export const COLORS = [
   },
 ] as const;
 
-export const MODELS = [
-  {
-    name: "models",
-    options: [
-      {
-        label: "iPhone X",
-        value: "iphonex",
-      },
-      {
-        label: "iPhone 11",
-        value: "iphone11",
-      },
-      {
-        label: "iPhone 12",
-        value: "iphone12",
-      },
-      {
-        label: "iPhone 13",
-        value: "iphone13",
-      },
-      {
-        label: "iPhone 14",
-        value: "iphone14",
-      },
-      {
-        label: "iPhone 15",
-        value: "iphone15",
-      },
-    ],
-  },
-] as const;
+export const MODELS = {
+  name: "models",
+  options: [
+    {
+      label: "iPhone X",
+      value: "iphonex",
+    },
+    {
+      label: "iPhone 11",
+      value: "iphone11",
+    },
+    {
+      label: "iPhone 12",
+      value: "iphone12",
+    },
+    {
+      label: "iPhone 13",
+      value: "iphone13",
+    },
+    {
+      label: "iPhone 14",
+      value: "iphone14",
+    },
+    {
+      label: "iPhone 15",
+      value: "iphone15",
+    },
+  ],
+} as const;
 
 export const MATERIALS = {
   name: "material",
