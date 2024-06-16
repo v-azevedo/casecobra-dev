8766bcba1347c56e483ce7391fea77b343fca2a9
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-06-16 20:48:35 -0300

    Implements webhook for stripe

---
+++ src/app/api/webhooks/route.ts
@@ -0,0 +1,78 @@
+import { db } from "@/db";
+import { stripe } from "@/lib/stripe";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.text();
+    const signature = req.headers.get("stripe-signature");
+
+    if (!signature) {
+      return new Response("No signature", { status: 400 });
+    }
+
+    const event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+
+    if (event.type === "checkout.session.completed") {
+      // handle checkout session completed
+      if (!event.data.object.customer_details?.email) {
+        throw new Error("Missing email in checkout session");
+      }
+
+      const session = event.data.object as Stripe.Checkout.Session;
+
+      const { userId, orderId } = session.metadata || {
+        orderId: null,
+        userId: null,
+      };
+
+      if (!userId || !orderId) {
+        throw new Error("Missing metadata in checkout session");
+      }
+
+      const billingAddress = session.customer_details!.address;
+      const shippingAddress = session.shipping_details!.address;
+
+      await db.order.update({
+        where: { id: orderId },
+        data: {
+          isPaid: true,
+          shippingAddress: {
+            create: {
+              name: session.customer_details!.name!,
+              city: shippingAddress!.city!,
+              country: shippingAddress!.country!,
+              postalCode: shippingAddress!.postal_code!,
+              street: shippingAddress!.line1!,
+              state: shippingAddress!.state,
+            },
+          },
+          billingAddress: {
+            create: {
+              name: session.customer_details!.name!,
+              city: billingAddress!.city!,
+              country: billingAddress!.country!,
+              postalCode: billingAddress!.postal_code!,
+              street: billingAddress!.line1!,
+              state: billingAddress!.state,
+            },
+          },
+        },
+      });
+    }
+
+    return NextResponse.json({ result: event, ok: true });
+  } catch (err) {
+    console.error(err);
+
+    return NextResponse.json(
+      { message: "Something went wrong", ok: false },
+      { status: 500 }
+    );
+  }
+}
