69107b3033da33bf0962199799709dd07828516f
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2024-06-18 14:05:28 -0300

    Update dashboard button link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -27,7 +27,7 @@ const NavBar = async () => {
                 </Link>
                 {isAdmin ? (
                   <Link
-                    href="/api/auth/logout"
+                    href="/dashboard"
                     className={buttonVariants({ size: "sm", variant: "ghost" })}
                   >
                     Dashboard
